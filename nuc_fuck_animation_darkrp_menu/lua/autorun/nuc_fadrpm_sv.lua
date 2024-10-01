b1448194b933f7ded5defef6ff22a1c0fb50181a
YohSambre <YohSambre@users.noreply.github.com>
2024-10-01 11:40:35 +0200

    Add files via upload

---
+++ nuc_fuck_animation_darkrp_menu/lua/autorun/nuc_fadrpm_sv.lua
@@ -0,0 +1,14 @@
+--------------------------------------------
+--*/ NUC JOIN THE BATTLE
+--------------------------------------------
+MsgC (Color(255, 255, 255), [[                                                                                                             
+= NUC - Fuck Animation DarkRP Menu loaded!
+]])
+
+--------------------------------------------
+--*/ BASE
+--------------------------------------------
+hook.Add( "Initialize", "NUC_FUCK_ANIMATION_DARKRP_MENU", function() 
+	concommand.Remove( "_DarkRP_AnimationMenu" )  
+end)
+
